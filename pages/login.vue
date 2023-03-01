2101615d9e72ed54f1d5c8aaefccb1cbda62a429
aaron mitchell <aaronmitchell9902@gmail.com>
2023-03-01 00:28:01 -0500

    login v1

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const email: Ref<string> = ref('');
+const password: Ref<string> = ref('');
+</script>
+
+<template>
+  <h2>Login</h2>
+  <div>
+    <form v-on:submit.prevent action="#" method="POST">
+
+      <label for="email">Email</label>
+      <input v-model="email" id="email" name="email" placeholder="king.james@lakers.com" type="email" required />
+
+      <label for="password">Password</label>
+      <input v-model="password" id="password" name="password" type="password" required />
+
+      <button type="submit">Submit</button>
+
+    </form>
+  </div>
+</template>
